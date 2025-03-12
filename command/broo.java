1f52466167574b282d9b9f63c59b2f7926193292
Gelix327 <gellish04@gmail.com>
2025-03-12 15:12:56 +0530

    addition

--- command/broo.java
+++ command/broo.java
@@ -1,6 +1,8 @@
 public class Broo{
     public static void main(String args[]){
         int num=55; //this is int value
-        System.out.println(num);
+        int num1=88;
+        int num3=num+num1;
+        System.out.println(num3);
     }
 }
