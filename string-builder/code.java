f07b5c110dd1c133f81c736d9b582e12582e76b1
Gelix327 <gellish04@gmail.com>
2025-03-13 00:11:36 +0530

    simple string

---
+++ string-builder/code.java
@@ -0,0 +1,11 @@
+public class code{
+    void codes(){
+        String a="Gellish ";
+        String b="Sagolshem";
+        System.out.println(a+b);
+    }
+    public static void main(String args[]){
+        code thing = new code();
+        thing.codes();
+    }
+}
