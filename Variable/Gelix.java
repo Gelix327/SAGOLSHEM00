2f232d8e46a97332e4c72f9afc10e6ff6b3a2614
Gelix327 <gellish04@gmail.com>
2025-03-12 14:01:34 +0530

    easy code

---
+++ Variable/Gelix.java
@@ -0,0 +1,6 @@
+public class Broo{
+    public static void main(String args[]){
+        int num1=55;
+        System.out.println("num1");
+    }
+}
---
+++ command/broo.java
@@ -0,0 +1,6 @@
+public class Broo{
+    public static void main(String args[]){
+        string num="99"
+        System.out.println(num);
+    }
+}
