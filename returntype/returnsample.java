c34692cf180fe0a144b7073e22593e7a18f94304
Gelix327 <gellish04@gmail.com>
2025-03-12 23:15:31 +0530

    program

---
+++ returntype/returnsample.java
@@ -0,0 +1,12 @@
+public class returnsample{
+    int add(int a, int b){
+         int c= a+b;
+    return c;
+}
+public static void main(String args[]){
+    returnsample obj = new returnsample();
+    int x= obj.add(22,3);
+    System.out.println(x);
+}
+    }
+   
