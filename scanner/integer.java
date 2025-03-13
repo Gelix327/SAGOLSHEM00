c28031b71e1549c0f0051d6643e5a5b3a550729e
Gelix327 <gellish04@gmail.com>
2025-03-13 15:10:36 +0530

    integernum

---
+++ scanner/integer.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class integer{
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter your age");
+        int age = input.nextInt();
+        System.out.println("my age is " + age);
+    }
+}
