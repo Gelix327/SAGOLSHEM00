f50567f35112bb5bae1c20af1a17882051ff0f2a
Gelix327 <gellish04@gmail.com>
2025-03-13 14:22:59 +0530

    oddnumberfind

---
+++ oddnumber/num1.java
@@ -0,0 +1,8 @@
+public class num1{
+    public static void main(String args[]){
+         for (int i =20;i<50;i++)
+            if(i%2 !=0){
+                System.out.println(i);
+            }
+         }
+    }
