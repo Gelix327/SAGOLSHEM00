b52d889ca75162bf3ec9ef9bbe483bd3dbc68e38
Gelix327 <gellish04@gmail.com>
2025-03-13 12:14:32 +0530

    arraynumber

---
+++ array/folder2.java
@@ -0,0 +1,7 @@
+public class folder2{
+    public static void main(String args[]){
+        int a[] ={100,34,56,789,99,56,78,11};
+        System.out.println(a[3]);
+        System.out.println(a[7]);
+    }
+}
