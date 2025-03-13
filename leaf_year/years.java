cda57f7435ee08371e3713ba402c411508f69eae
Gelix327 <gellish04@gmail.com>
2025-03-13 14:31:45 +0530

    leafyearsearch

---
+++ leaf_year/years.java
@@ -0,0 +1,11 @@
+public class years{
+    public static void main(String args[]){
+         int year = 2004;
+            if(year%4==0){
+                System.out.println(year +"is a leaf year");
+            }
+            else{
+                System.out.println(year +"is not a leaf year");
+            }
+         }
+    }
