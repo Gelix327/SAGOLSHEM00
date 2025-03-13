b1806f3c0290d26acab320b63de4637c55037284
Gelix327 <gellish04@gmail.com>
2025-03-13 10:59:40 +0530

    ifcondition

---
+++ ifelse/country.java
@@ -0,0 +1,10 @@
+public class country{
+    public static void main(String args[]){
+        if(true){
+            System.out.println("I m the men");
+        }
+        else;{
+            System.out.println("I m not men");
+        }
+    }
+}
