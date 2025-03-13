c43e6676415bd69ace0624ca9374a7942eea8ae7
Gelix327 <gellish04@gmail.com>
2025-03-13 11:34:49 +0530

    positivenegative

---
+++ positivenegative/folder1.java
@@ -0,0 +1,13 @@
+public class folder1{
+public static void main(String args[]){
+        int a = 25;
+        if(a<=0){
+            System.out.println("negative");
+
+        }
+        else{
+            System.out.println("positive");
+        }
+
+    }
+}
