c9dcd69bad585b1d3659c5056066f171ecf9e880
Gelix327 <gellish04@gmail.com>
2025-03-14 11:56:43 +0530

    finding legth of sentences

---
+++ length/sample.java
@@ -0,0 +1,9 @@
+package length;
+import java.util.*;
+public class sample {
+    public static void main(String args[]){
+        String name = "welcome,newbies";
+        System.out.println(name.length());
+    }
+    
+}
