402635297c2b9fa69721589b883d61663a10d0ca
Gelix327 <gellish04@gmail.com>
2025-03-14 10:15:10 +0530

    program for while loop

---
+++ dynamic/folder3.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class folder3{
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+        System.out.println("choose the option ");
+        System.out.println("option 1: addition \n option 2: substraction \n ");
+        int choose = input.nextInt();
+    }
+}
