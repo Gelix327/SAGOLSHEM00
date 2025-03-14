1273a16e665d756bb1fce7c362f8e9455ae2f48c
Gelix327 <gellish04@gmail.com>
2025-03-14 12:04:23 +0530

    UpperLowecase

---
+++ caseupperlower/Upper.java
@@ -0,0 +1,9 @@
+
+import java.util.*;
+public class Upper {
+    public static void main(String args[]){
+        String name ="welcome,newbies";
+        System.out.println(name.toUpperCase());
+    }
+    
+}
