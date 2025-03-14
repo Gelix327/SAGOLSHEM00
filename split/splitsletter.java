a98aa5731c12ee439e16d88b9536a9b9f4e3f300
Gelix327 <gellish04@gmail.com>
2025-03-14 11:49:55 +0530

    splitletterfinding

---
+++ split/splitsletter.java
@@ -0,0 +1,10 @@
+package split;
+import java.util.*;
+public class splitsletter {
+    public static void main(String args[]){
+        String name = "welcome,newbies";
+        String names[] = name.split(",");
+        System.out.println(names[1]);
+    }
+    
+}
