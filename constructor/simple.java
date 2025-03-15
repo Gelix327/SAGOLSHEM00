9ea0ec9ec54c33d65162585c174eadf82ad1c7b2
Gelix327 <gellish04@gmail.com>
2025-03-15 11:45:47 +0530

    program

---
+++ constructor/simple.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class simple {
+     void simple(){
+        System.out.println("welcome");
+    }
+    public static void main(String args[]){
+        simple obj =  new simple();
+        obj.simple();
+    }
+    
+}
